d2eaf37b1b1f1a2ef8729f699784eed29c5a4236
yb8bo <704347176@qq.com>
2020-03-25 16:30:32 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("1");
+        System.out.println("2");
+    }
+}
